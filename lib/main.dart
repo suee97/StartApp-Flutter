3ebd3d0f5614168bfb37a6abd376d05b8221d335
suee97 <atn1su@gmail.com>
2022-07-12 15:48:58 +0900

    refactor: refactor splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 import 'package:start_app/notifiers/event_notifier.dart';
 import 'screens/home/home_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized(); /// 비동기 처리
+  await dotenv.load(fileName: ".env"); /// .env 파일 로딩
   runApp(const MyApp());
 }
 
@@ -16,7 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (_) => EventNotifier())
+        ChangeNotifierProvider(create: (_) => EventNotifier()) /// 이벤트 notifier
       ],
       child: MaterialApp(
         title: '서울과학기술대학교 총학생회',
