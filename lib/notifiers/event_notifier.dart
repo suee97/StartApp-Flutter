3cc36fc06a8cd2cae2176ba3e173946a852371d9
suee97 <atn1su@gmail.com>
2022-07-15 05:03:50 +0900

    gitignore

--- lib/notifiers/event_notifier.dart
+++ lib/notifiers/event_notifier.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:start_app/models/event_list.dart';
 
 class EventNotifier extends ChangeNotifier {
-  List<Event> _eventList = [
+  final List<Event> _eventListDummy = [
     Event(1, "그때 그 시절", "https://docs.google.com/forms/d/e/1FAIpQLSe0NjSatm6bLuJHayA8BSNOEnCdQ6RxjRgJRFO_85Q6f4A8yg/viewform", "https://img", "#ffff00",
         "2022-08-05T22:23:21.159220", "2022-08-06T22:23:21.159220", false),
     Event(2, "축제 미션 올 클리어", "https://docs.google.com/forms/d/e/1FAIpQLSe0NjSatm6bLuJHayA8BSNOEnCdQ6RxjRgJRFO_85Q6f4A8yg/viewform", "https://img", "#ff12ff",
@@ -22,8 +22,9 @@ class EventNotifier extends ChangeNotifier {
   ];
 
   List<Event> getEvents() {
-    return _eventList;
+    return _eventListDummy;
   }
 
   Future<void> fetchEvents() async {}
+
 }
