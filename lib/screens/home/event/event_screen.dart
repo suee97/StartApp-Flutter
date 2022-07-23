58b393132791feacd7b59e5bbe462e96ff1cc6bf
suee97 <atn1su@gmail.com>
2022-07-23 12:46:37 +0900

    [REFACTOR] event screen ui & event screen error handling

--- lib/screens/home/event/event_screen.dart
+++ lib/screens/home/event/event_screen.dart
@@ -18,11 +18,8 @@ class EventScreen extends StatefulWidget {
 }
 
 class _EventScreenState extends State<EventScreen> {
-  late bool isLoading;
-
   @override
   void initState() {
-    isLoading = true;
     fetchEventList();
     super.initState();
   }
@@ -30,16 +27,18 @@ class _EventScreenState extends State<EventScreen> {
   Future<List<Event>> fetchEventList() async {
     await Future.delayed(const Duration(seconds: 2));
     try {
-      var resString =
-          await http.get(Uri.parse("${dotenv.get("LOCAL_API_BASE_URL")}/list"));
+      var resString = await http
+          .get(Uri.parse("${dotenv.get("LOCAL_API_BASE_URL")}/list"))
+          .timeout(const Duration(seconds: 5), onTimeout: () {
+        return Future.error("error timeout");
+      });
       Map<String, dynamic> resData = jsonDecode(resString.body);
       List<dynamic> data = resData["data"];
       List<Event> _eventList = [];
       _eventList = data.map((e) => Event.fromJson(e)).toList();
 
       if (resData["status"] == 404) {
-        return Future.error(
-            "#### status code : ${resData["status"]}\n#### message : ${resData["message"]}");
+        return Future.error("error 404");
       } else {
         return _eventList;
       }
@@ -58,86 +57,100 @@ class _EventScreenState extends State<EventScreen> {
         title: const Text("서울과학기술대학교 총학생회"),
       ),
       body: Container(
-        color: HexColor("#f3f3f3"),
-        child: FutureBuilder(
-          future: fetchEventList(),
-          builder: (BuildContext context, AsyncSnapshot snapshot) {
-            if (snapshot.hasData && snapshot.data.length != 0) {
-              return Container(
-                  decoration: BoxDecoration(
-                      color: HexColor("#425C5A"),
-                      borderRadius: const BorderRadius.only(
-                        topLeft: Radius.circular(30),
-                        topRight: Radius.circular(30),
-                      )),
-                  child: Column(children: <Widget>[
-                    SizedBox(
-                      height: 23.h,
-                    ),
-                    Container(
-                      margin: const EdgeInsets.only(left: 26),
-                      width: double.infinity,
-                      child: Text(
-                        "이벤트 참여",
-                        style: TextStyle(
-                            fontSize: 24.sp,
-                            fontWeight: FontWeight.w700,
-                            color: HexColor("#92AEAC")),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 13.h,
-                    ),
-                    Expanded(
-                      child: ListView.builder(
-                        itemCount: snapshot.data.length,
-                        itemBuilder: (context, index) {
-                          return EventTile(
-                              event: snapshot.data[index],
-                              onPressed: () => {
-                                    Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (context) =>
-                                                EventDetailScreen(
-                                                    event:
-                                                        snapshot.data[index])))
-                                  });
-                        },
-                      ),
-                    )
-                  ]));
-            } else if (snapshot.hasData && snapshot.data.length == 0) {
-              return Text("진행중인 이벤트가 없습니다.");
-            } else if (snapshot.hasData == false) {
-              return Container(
-                decoration: BoxDecoration(
-                    color: HexColor("#425C5A"),
-                    borderRadius: const BorderRadius.only(
-                      topLeft: Radius.circular(30),
-                      topRight: Radius.circular(30),
-                    )),
-                child: Container(
+          color: HexColor("#f3f3f3"),
+          child: Container(
+              decoration: BoxDecoration(
+                  color: HexColor("#425C5A"),
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
+                  )),
+              child: Column(children: <Widget>[
+                SizedBox(
+                  height: 23.h,
+                ),
+                Container(
+                  margin: const EdgeInsets.only(left: 26),
                   width: double.infinity,
-                  height: double.infinity,
-                  child: Center(
-                    child: Platform.isIOS
-                        ? CupertinoActivityIndicator(
-                            color: HexColor("#f3f3f3"),
-                            radius: 12,
-                          )
-                        : CircularProgressIndicator(
-                            color: HexColor("#f3f3f3"),
-                          ),
+                  child: Text(
+                    "이벤트 참여",
+                    style: TextStyle(
+                        fontSize: 24.sp,
+                        fontWeight: FontWeight.w700,
+                        color: HexColor("#92AEAC")),
                   ),
                 ),
-              );
-            } else {
-              return Text("에러 발생");
-            }
-          },
-        ),
-      ),
+                SizedBox(
+                  height: 13.h,
+                ),
+                FutureBuilder(
+                  future: fetchEventList(),
+                  builder: (BuildContext context, AsyncSnapshot snapshot) {
+                    if (snapshot.hasData && snapshot.data.length != 0) {
+                      return Expanded(
+                        child: ListView.builder(
+                          itemCount: snapshot.data.length,
+                          itemBuilder: (context, index) {
+                            return EventTile(
+                                event: snapshot.data[index],
+                                onPressed: () => {
+                                      Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) =>
+                                                  EventDetailScreen(
+                                                      event: snapshot
+                                                          .data[index])))
+                                    });
+                          },
+                        ),
+                      );
+                    } else if (snapshot.hasData && snapshot.data.length == 0) {
+                      return Column(
+                        children: [
+                          SizedBox(
+                            height: 200.h,
+                          ),
+                          Text(
+                            "진행중인 이벤트가 없습니다.",
+                            style: TextStyle(
+                                fontSize: 14, color: HexColor("#f3f3f3")),
+                          ),
+                        ],
+                      );
+                    } else if (snapshot.hasError) {
+                      return Column(
+                        children: [
+                          SizedBox(
+                            height: 200.h,
+                          ),
+                          Text(
+                            "정보를 불러오지 못했습니다.",
+                            style: TextStyle(
+                                fontSize: 14, color: HexColor("#f3f3f3")),
+                          ),
+                        ],
+                      );
+                    } else {
+                      return Column(
+                        children: [
+                          SizedBox(
+                            height: 200.h,
+                          ),
+                          Platform.isIOS
+                              ? CupertinoActivityIndicator(
+                                  color: HexColor("#f3f3f3"),
+                                  radius: 12,
+                                )
+                              : CircularProgressIndicator(
+                                  color: HexColor("#f3f3f3"),
+                                ),
+                        ],
+                      );
+                    }
+                  },
+                )
+              ]))),
     );
   }
 }
