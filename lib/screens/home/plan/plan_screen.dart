7faa48a1d948ae77694ac98abea3c2c7ad11f809
KangInyeong <kiyoog02@gmail.com>
2022-07-25 22:44:20 +0900

    [FEAT] create calendar on plan screen

--- lib/screens/home/plan/plan_screen.dart
+++ lib/screens/home/plan/plan_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:start_app/screens/home/plan/Meeting.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
 
 class PlanScreen extends StatelessWidget {
   const PlanScreen({Key? key}) : super(key: key);
@@ -6,10 +8,76 @@ class PlanScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("plan screen"),),
-      body: Center(
-        child: Text("plan screen"),
+      appBar: AppBar(title: Text("학사 일정"),),
+      body: SfCalendar(
+        view: CalendarView.month,
+        dataSource: MeetingDataSource(_getDataSource()),
+        monthViewSettings: MonthViewSettings(showAgenda: true),
       ),
     );
+  }
+
+  List<Meeting> _getDataSource() {
+    final List<Meeting> meetings = <Meeting>[];
+    final DateTime today = DateTime.now();
+
+    final DateTime startTime = DateTime(today.year, today.month, today.day, 9);
+    final DateTime endTime = startTime.add(const Duration(hours: 2));
+
+    final DateTime startTime1 = DateTime(today.year, today.month, today.day, 10);
+    final DateTime endTime1 = startTime.add(const Duration(hours: 3));
+
+    final DateTime startTime2 = DateTime(today.year, today.month, today.day-2);
+
+    meetings.add(Meeting(
+        'Conference', startTime, endTime, const Color(0xFF0F8644), false));
+    meetings.add(Meeting(
+        'ST', startTime1, endTime1, const Color(0xFF0F3344), false));
+    meetings.add(Meeting(
+        'GO', startTime2, endTime, const Color(0xFF0F3374), false));
+    meetings.add(Meeting(
+        'HLO', startTime, endTime, const Color(0xFF0C1784), false));
+    return meetings;
+  }
+}
+
+class MeetingDataSource extends CalendarDataSource {
+  MeetingDataSource(List<Meeting> source) {
+    appointments = source;
+  }
+
+  @override
+  DateTime getStartTime(int index) {
+    return _getMeetingData(index).from;
+  }
+
+  @override
+  DateTime getEndTime(int index) {
+    return _getMeetingData(index).to;
+  }
+
+  @override
+  String getSubject(int index) {
+    return _getMeetingData(index).eventName;
+  }
+
+  @override
+  Color getColor(int index) {
+    return _getMeetingData(index).background;
+  }
+
+  @override
+  bool isAllDay(int index) {
+    return _getMeetingData(index).isAllDay;
+  }
+
+  Meeting _getMeetingData(int index) {
+    final dynamic meeting = appointments![index];
+    late final Meeting meetingData;
+    if (meeting is Meeting) {
+      meetingData = meeting;
+    }
+
+    return meetingData;
   }
 }
