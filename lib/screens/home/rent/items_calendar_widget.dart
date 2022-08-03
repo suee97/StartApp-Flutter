c186a3e11918d89cbf6a39a6247cb7233139c6bc
KangInyeong <kiyoog02@gmail.com>
2022-08-03 11:07:11 +0900

    [ADD] plan screen ui style

---
+++ lib/screens/home/rent/items_calendar_widget.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ItemsCalendarWidget extends StatelessWidget{
+
+  const ItemsCalendarWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("sfcalendar");
+  }
+
+
+}
