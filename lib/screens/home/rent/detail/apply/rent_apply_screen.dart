7af23c496e2047c23aa21638faf39210f872f228
suee97 <atn1su@gmail.com>
2022-08-04 02:17:03 +0900

    [ADD] dotted line widget & package

--- lib/screens/home/rent/detail/apply/rent_apply_screen.dart
+++ lib/screens/home/rent/detail/apply/rent_apply_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:hexcolor/hexcolor.dart';
 import '../../../../../utils/common.dart';
+import 'dotted_line_widget.dart';
 
 class RentApplyScreen extends StatelessWidget {
   const RentApplyScreen({Key? key}) : super(key: key);
@@ -18,6 +21,30 @@ class RentApplyScreen extends StatelessWidget {
         centerTitle: true,
         elevation: 0,
       ),
+      body: Stack(
+        children: [
+          Container(
+              width: double.infinity,
+              height: double.infinity,
+              margin: EdgeInsets.only(left: 10.w, right: 10.w, bottom: 10.h),
+              child: SvgPicture.asset(
+                "assets/rent_apply_background.svg",
+                fit: BoxFit.fill,
+              )),
+          Column(
+            children: [
+              SizedBox(height: 100.h,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  DottedLineWidget(),
+                ],
+              )
+            ],
+          )
+        ],
+      ),
+      backgroundColor: HexColor("#425c5a"),
     );
   }
 }
