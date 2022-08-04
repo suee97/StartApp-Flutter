2cb295d3a76d4209eb53643a0f22a5d783f86448
suee97 <atn1su@gmail.com>
2022-08-04 22:33:42 +0900

    [FEAT] logout

--- lib/screens/home/profile/profile_screen.dart
+++ lib/screens/home/profile/profile_screen.dart
@@ -1,8 +1,13 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:start_app/screens/home/home_screen.dart';
 import 'package:start_app/widgets/test_button.dart';
-
+import 'package:http/http.dart' as http;
 import '../../../utils/common.dart';
 import '../../splash/splash_screen.dart.dart';
 
@@ -29,17 +34,44 @@ class ProfileScreen extends StatelessWidget {
             },
             title: "set nonlogin false",
           ),
-          TestButton(title: "logout", onPressed: () async {
-            /// 로그아웃
-            final secureStorage = FlutterSecureStorage();
-            secureStorage.write(key: "ACCESS_TOKEN", value: "");
-            secureStorage.write(key: "REFRESH_TOKEN", value: "");
-            Common.setAutoLogin(false);
-            Common.setNonLogin(false);
-            Navigator.of(context).pushAndRemoveUntil(
-                MaterialPageRoute(builder: (context) => SplashScreen()),
+          TestButton(
+              title: "logout",
+              onPressed: () async {
+                /// 로그아웃
+
+                var secureStorage = FlutterSecureStorage();
+                var ACCESS_TOKEN =
+                    await secureStorage.read(key: "ACCESS_TOKEN");
+                var REFRESH_TOKEN =
+                    await secureStorage.read(key: "REFRESH_TOKEN");
+
+                Map<String, dynamic> resData91 = {};
+                resData91["status"] = 400;
+                try {
+                  var resString = await http.get(
+                      Uri.parse(
+                          "${dotenv.get("DEV_API_BASE_URL")}/auth/logout"),
+                      headers: {
+                        "Authorization": "Bearer $ACCESS_TOKEN",
+                        "Refresh": "Bearer $REFRESH_TOKEN"
+                      }).timeout(const Duration(seconds: 10));
+                  resData91 = jsonDecode(utf8.decode(resString.bodyBytes));
+                } on TimeoutException catch (e) {
+                  print(e);
+                } on SocketException catch (e) {
+                  print(e);
+                } catch (e) {
+                  print(e);
+                }
+                secureStorage.write(key: "ACCESS_TOKEN", value: "");
+                secureStorage.write(key: "REFRESH_TOKEN", value: "");
+                Common.setAutoLogin(false);
+                Common.setNonLogin(false);
+
+                Navigator.of(context).pushAndRemoveUntil(
+                    MaterialPageRoute(builder: (context) => SplashScreen()),
                     (route) => false);
-          })
+              })
         ],
       ),
     );
