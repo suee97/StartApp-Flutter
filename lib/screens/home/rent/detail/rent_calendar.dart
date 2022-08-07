4ac2003e171a9c994d4b4b21e3f943db7d98119e
suee97 <atn1su@gmail.com>
2022-08-07 21:30:29 +0900

    [REFACTOR] arrow tap size

--- lib/screens/home/rent/detail/rent_calendar.dart
+++ lib/screens/home/rent/detail/rent_calendar.dart
@@ -15,7 +15,6 @@ class RentCalendar extends StatefulWidget {
 }
 
 class _RentCalendarState extends State<RentCalendar> {
-
   CalendarController _calendarController = CalendarController();
   final DateTime today = DateTime.now();
   late String _headerText = today.month.toString();
@@ -23,101 +22,118 @@ class _RentCalendarState extends State<RentCalendar> {
   @override
   Widget build(BuildContext context) {
     return Column(children: [
-    Container(
-    width: 180.w,
-    height: 20.h,
-      decoration: BoxDecoration(
-          color: HexColor("#FFCEA2"),
-          borderRadius: const BorderRadius.all(Radius.circular(10)
-    )),
-    child: Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          GestureDetector(
-            onTap: () {
-              _calendarController.backward!();
-            },
-            child: SvgPicture.asset('assets/back_btn.svg', width: 10.w, height: 10.w, fit: BoxFit.contain),
-          ),
-          // IconButton(icon: Icon(Icons.arrow_back),
-          //   onPressed: () {
-          //   _calendarController.backward!();
-          //   },),
-          Text(_headerText,
+      Container(
+        width: 180.w,
+        height: 20.h,
+        decoration: BoxDecoration(
+            color: HexColor("#FFCEA2"),
+            borderRadius: const BorderRadius.all(Radius.circular(10))),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            GestureDetector(
+              onTap: () {
+                _calendarController.backward!();
+              },
+              child: Container(
+                  width: 20.w,
+                  height: 18.h,
+                  child: Padding(
+                    padding: EdgeInsets.only(left: 6.w, right: 6.w, top: 4.h, bottom: 4.h),
+                    child: SvgPicture.asset(
+                      'assets/back_btn.svg',
+                      fit: BoxFit.fill,
+                    ),
+                  )),
+            ),
+            // IconButton(icon: Icon(Icons.arrow_back),
+            //   onPressed: () {
+            //   _calendarController.backward!();
+            //   },),
+            Text(_headerText,
                 textAlign: TextAlign.center,
                 style: TextStyle(fontSize: 11.5.sp)),
-          GestureDetector(
-            onTap: () {
-              _calendarController.forward!();
-            },
-            child: SvgPicture.asset('assets/forward_btn.svg', width: 10.w, height: 10.w, fit: BoxFit.contain),
-          ),
-          // IconButton(icon: Icon(Icons.arrow_forward),
-          //   onPressed: () {
-          //   _calendarController.forward!();
-          //   },)
-        ],
-      ),),
+            GestureDetector(
+              onTap: () {
+                _calendarController.forward!();
+              },
+                child: Container(
+                    width: 20.w,
+                    height: 18.h,
+                    child: Padding(
+                      padding: EdgeInsets.only(left: 6.w, right: 6.w, top: 4.h, bottom: 4.h),
+                      child: SvgPicture.asset(
+                        'assets/forward_btn.svg',
+                        fit: BoxFit.fill,
+                      ),
+                    )),
+            ),
+            // IconButton(icon: Icon(Icons.arrow_forward),
+            //   onPressed: () {
+            //   _calendarController.forward!();
+            //   },)
+          ],
+        ),
+      ),
       Container(
-      height: 330.h,
-      child: SfCalendar(
-      view: CalendarView.month,
-      controller: _calendarController,
-        headerHeight: 0,
-        onViewChanged: (ViewChangedDetails viewChangedDetails) {
-          if (_calendarController.view == CalendarView.month) {
-            _headerText = DateFormat('MMMM')
-                .format(viewChangedDetails
-                .visibleDates[viewChangedDetails.visibleDates.length ~/ 2])
-                .toString();
-          }
-          if(_headerText == 'January'){
-            _headerText = "1월 예약 현황";
-          }else if(_headerText == 'February'){
-            _headerText = "2월 예약 현황";
-          }else if(_headerText == 'March'){
-            _headerText = "3월 예약 현황";
-          }else if(_headerText == 'April'){
-            _headerText = "4월 예약 현황";
-          }else if(_headerText == 'May'){
-            _headerText = "5월 예약 현황";
-          }else if(_headerText == 'June'){
-            _headerText = "6월 예약 현황";
-          }else if(_headerText == 'July'){
-            _headerText = "7월 예약 현황";
-          }else if(_headerText == 'August'){
-            _headerText = "8월 예약 현황";
-          }else if(_headerText == 'September'){
-            _headerText = "9월 예약 현황";
-          }else if(_headerText == 'October'){
-            _headerText = "10월 예약 현황";
-          }else if(_headerText == 'November'){
-            _headerText = "11월 예약 현황";
-          }else if(_headerText == 'December'){
-            _headerText = "12월 예약 현황";
-          }
+        height: 330.h,
+        child: SfCalendar(
+          view: CalendarView.month,
+          controller: _calendarController,
+          headerHeight: 0,
+          onViewChanged: (ViewChangedDetails viewChangedDetails) {
+            if (_calendarController.view == CalendarView.month) {
+              _headerText = DateFormat('MMMM')
+                  .format(viewChangedDetails.visibleDates[
+                      viewChangedDetails.visibleDates.length ~/ 2])
+                  .toString();
+            }
+            if (_headerText == 'January') {
+              _headerText = "1월 예약 현황";
+            } else if (_headerText == 'February') {
+              _headerText = "2월 예약 현황";
+            } else if (_headerText == 'March') {
+              _headerText = "3월 예약 현황";
+            } else if (_headerText == 'April') {
+              _headerText = "4월 예약 현황";
+            } else if (_headerText == 'May') {
+              _headerText = "5월 예약 현황";
+            } else if (_headerText == 'June') {
+              _headerText = "6월 예약 현황";
+            } else if (_headerText == 'July') {
+              _headerText = "7월 예약 현황";
+            } else if (_headerText == 'August') {
+              _headerText = "8월 예약 현황";
+            } else if (_headerText == 'September') {
+              _headerText = "9월 예약 현황";
+            } else if (_headerText == 'October') {
+              _headerText = "10월 예약 현황";
+            } else if (_headerText == 'November') {
+              _headerText = "11월 예약 현황";
+            } else if (_headerText == 'December') {
+              _headerText = "12월 예약 현황";
+            }
 
-          SchedulerBinding.instance.addPostFrameCallback((duration) {
-            setState(() {});
-          });
-        },
-      monthViewSettings: const MonthViewSettings(
-          appointmentDisplayCount: 4,
-          appointmentDisplayMode:
-              MonthAppointmentDisplayMode.appointment,
-          monthCellStyle: MonthCellStyle(
-              textStyle: TextStyle(color: Colors.black))),
-      dataSource: MeetingDataSource(_getDataSource()),
-      todayHighlightColor: Colors.orange,
-      todayTextStyle: const TextStyle(
-          color: Colors.black, fontWeight: FontWeight.w700),
-      selectionDecoration: const BoxDecoration(),
-      cellBorderColor: HexColor("#425c5a"),
-    ),
-    )]);
+            SchedulerBinding.instance.addPostFrameCallback((duration) {
+              setState(() {});
+            });
+          },
+          monthViewSettings: const MonthViewSettings(
+              appointmentDisplayCount: 4,
+              appointmentDisplayMode: MonthAppointmentDisplayMode.appointment,
+              monthCellStyle:
+                  MonthCellStyle(textStyle: TextStyle(color: Colors.black))),
+          dataSource: MeetingDataSource(_getDataSource()),
+          todayHighlightColor: Colors.orange,
+          todayTextStyle:
+              const TextStyle(color: Colors.black, fontWeight: FontWeight.w700),
+          selectionDecoration: const BoxDecoration(),
+          cellBorderColor: HexColor("#425c5a"),
+        ),
+      )
+    ]);
   }
-
 }
 
 List<Meeting> _getDataSource() {
