5f2280fa9607d5c4202f7714b881b3068883c137
suee97 <atn1su@gmail.com>
2022-08-09 03:12:50 +0900

    [REFACTOR] my rent sctile ui

--- lib/screens/home/rent/my_rent/my_rent_screen.dart
+++ lib/screens/home/rent/my_rent/my_rent_screen.dart
@@ -6,7 +6,6 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:hexcolor/hexcolor.dart';
-import 'package:logger/logger.dart';
 import 'package:start_app/screens/home/rent/my_rent/rent_tile.dart';
 import 'package:http/http.dart' as http;
 import 'dart:io' show Platform, SocketException;
@@ -21,9 +20,6 @@ class MyRentScreen extends StatefulWidget {
 }
 
 class _MyRentScreenState extends State<MyRentScreen> {
-
-  var logger = Logger();
-
   @override
   void initState() {
     fetchRentList();
@@ -35,7 +31,8 @@ class _MyRentScreenState extends State<MyRentScreen> {
       var resString = await http
           .get(Uri.parse("${dotenv.get("DEV_API_BASE_URL")}/member/rent"))
           .timeout(const Duration(seconds: 10));
-      Map<String, dynamic> resData = jsonDecode(utf8.decode(resString.bodyBytes));
+      Map<String, dynamic> resData =
+          jsonDecode(utf8.decode(resString.bodyBytes));
 
       List<Rent> _rentList = [];
       List<Rent> _tempList = [];
@@ -62,7 +59,6 @@ class _MyRentScreenState extends State<MyRentScreen> {
         }
       } else {
         /// null
-        logger.d("${resData["status"]}");
         return Future.error("");
       }
 
@@ -80,8 +76,10 @@ class _MyRentScreenState extends State<MyRentScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("내 예약",
-          style: Common.startAppBarTextStyle,),
+        title: Text(
+          "내 예약",
+          style: Common.startAppBarTextStyle,
+        ),
         backgroundColor: HexColor("#425C5A"),
         foregroundColor: Colors.white,
         elevation: 0,
@@ -89,161 +87,121 @@ class _MyRentScreenState extends State<MyRentScreen> {
       ),
       body: Stack(children: [
         Container(
-          width: double.infinity,
-          height: double.infinity,
-          margin: EdgeInsets.only(top: 55.h),
-          decoration: BoxDecoration(
-              color: HexColor("#f3f3f3"),
-              borderRadius: const BorderRadius.only(
-                topLeft: Radius.circular(30),
-                topRight: Radius.circular(30),
-              )),
-          child: Container(
-            padding: EdgeInsets.only(top: 72.h, left: 20.w, right: 20.w),
-            child:Column(
-            children: [
-              Stack(
+            width: double.infinity,
+            height: double.infinity,
+            margin: EdgeInsets.only(top: 55.h),
+            decoration: BoxDecoration(
+                color: HexColor("#f3f3f3"),
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(30),
+                  topRight: Radius.circular(30),
+                )),
+            child: Container(
+              padding: EdgeInsets.only(top: 72.h, left: 20.w, right: 20.w),
+              child: Column(
                 children: [
-                  SvgPicture.asset(
-                    "assets/rent_tile.svg",
-                    fit: BoxFit.fill,
-                    width: 320.w,
-                    height: 60.h,
+
+                  /// RENT TILE UI TEST
+                  RentTile2(
+                      rent: Rent(1, "TABLE", "2022-08-05T22:23:21.159220",
+                          "2022-08-07T22:23:21.159220", 5, "CONFIRM"),
+                      onPressed: () {}),
+                  /// RENT TILE UI TEST
+
+                  FutureBuilder(
+                    future: fetchRentList(),
+                    builder: (BuildContext context, AsyncSnapshot snapshot) {
+                      if (snapshot.hasData && snapshot.data.length != 0) {
+                        return Expanded(
+                          child: ListView.builder(
+                            itemCount: snapshot.data.length,
+                            itemBuilder: (context, index) {
+                              return RentTile(
+                                  rent: snapshot.data[index],
+                                  onPressed: () => {});
+                            },
+                          ),
+                        );
+                      } else if (snapshot.hasData &&
+                          snapshot.data.length == 0) {
+                        return Column(
+                          children: [
+                            SizedBox(
+                              height: 120.h,
+                            ),
+                            Container(
+                              width: 132.w,
+                              child: const Image(
+                                  fit: BoxFit.fitWidth,
+                                  image:
+                                      AssetImage("images/logo_wink_ready.png")),
+                            ),
+                            SizedBox(
+                              height: 16.h,
+                            ),
+                            Text(
+                              "예약된 물품이 없습니다.",
+                              style: TextStyle(
+                                  fontSize: 20.sp,
+                                  color:
+                                      const Color.fromARGB(128, 216, 232, 231),
+                                  fontWeight: FontWeight.w700),
+                              textAlign: TextAlign.center,
+                            ),
+                          ],
+                        );
+                      } else if (snapshot.hasError) {
+                        return Column(
+                          children: [
+                            SizedBox(
+                              height: 120.h,
+                            ),
+                            Container(
+                              width: 132.w,
+                              child: Image(
+                                fit: BoxFit.fitWidth,
+                                image:
+                                    AssetImage("images/logo_crying_ready.png"),
+                                color: Colors.white.withOpacity(0.5),
+                                colorBlendMode: BlendMode.modulate,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 4.h,
+                            ),
+                            Text(
+                              "예약 정보를 불러오지 못했습니다.",
+                              style: TextStyle(
+                                  fontSize: 20.sp,
+                                  color:
+                                      const Color.fromARGB(128, 216, 232, 231),
+                                  fontWeight: FontWeight.w700),
+                              textAlign: TextAlign.center,
+                            ),
+                          ],
+                        );
+                      } else {
+                        return Column(
+                          children: [
+                            SizedBox(
+                              height: 200.h,
+                            ),
+                            Platform.isIOS
+                                ? CupertinoActivityIndicator(
+                                    color: HexColor("#f3f3f3"),
+                                    radius: 12,
+                                  )
+                                : CircularProgressIndicator(
+                                    color: HexColor("#f3f3f3"),
+                                  ),
+                          ],
+                        );
+                      }
+                    },
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        margin: EdgeInsets.only(left: 17.w),
-                        padding: EdgeInsets.only(top: 10.h, bottom: 10.h),
-                        height: 60.h,
-                        child:
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Row(
-                            children: [
-                              Row(
-                                children: [
-                                  Text("이름 : ", style: TextStyle(fontSize: 15.5.sp, fontWeight: FontWeight.w500),),
-                                  Text("엠프&마이크")
-                                ],
-                              ),
-                              SizedBox(width: 8.w,),
-                              Row(
-                                children: [
-                                  Text("수량 : ", style: TextStyle(fontSize: 15.5.sp, fontWeight: FontWeight.w500),),
-                                  Text("2")
-                                ],
-                              ),
-                            ],
-                          ),
-                          Row(
-                            children: [
-                              Text("대여기간 : ", style: TextStyle(fontSize: 15.5.sp, fontWeight: FontWeight.w500),),
-                              Text("00.00.00 ~ 00.00.00")
-                            ],
-                          ),
-                        ],
-                      ),),
-                      Padding(
-                          padding:EdgeInsets.only(right: 31),
-                          child: Text("승인"))
-                    ],
-                  )
                 ],
               ),
-              FutureBuilder(
-                future: fetchRentList(),
-                builder: (BuildContext context, AsyncSnapshot snapshot) {
-                  if (snapshot.hasData && snapshot.data.length != 0) {
-                    return Expanded(
-                      child: ListView.builder(
-                        itemCount: snapshot.data.length,
-                        itemBuilder: (context, index) {
-                          return RentTile(
-                              rent: snapshot.data[index],
-                              onPressed: () => {});
-                        },
-                      ),
-                    );
-                  } else if (snapshot.hasData && snapshot.data.length == 0) {
-                    return Column(
-                      children: [
-                        SizedBox(
-                          height: 120.h,
-                        ),
-                        Container(
-                          width: 132.w,
-                          child: const Image(
-                              fit: BoxFit.fitWidth,
-                              image:
-                              AssetImage("images/logo_wink_ready.png")),
-                        ),
-                        SizedBox(
-                          height: 16.h,
-                        ),
-                        Text(
-                          "예약된 물품이 없습니다.",
-                          style: TextStyle(
-                              fontSize: 20.sp,
-                              color: const Color.fromARGB(128, 216, 232, 231),
-                              fontWeight: FontWeight.w700),
-                          textAlign: TextAlign.center,
-                        ),
-                      ],
-                    );
-                  } else if (snapshot.hasError) {
-                    return Column(
-                      children: [
-                        SizedBox(
-                          height: 120.h,
-                        ),
-                        Container(
-                          width: 132.w,
-                          child: Image(
-                            fit: BoxFit.fitWidth,
-                            image: AssetImage("images/logo_crying_ready.png"),
-                            color: Colors.white.withOpacity(0.5),
-                            colorBlendMode: BlendMode.modulate,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 4.h,
-                        ),
-                        Text(
-                          "예약 정보를 불러오지 못했습니다.",
-                          style: TextStyle(
-                              fontSize: 20.sp,
-                              color: const Color.fromARGB(128, 216, 232, 231),
-                              fontWeight: FontWeight.w700),
-                          textAlign: TextAlign.center,
-                        ),
-                      ],
-                    );
-                  } else {
-                    return Column(
-                      children: [
-                        SizedBox(
-                          height: 200.h,
-                        ),
-                        Platform.isIOS
-                            ? CupertinoActivityIndicator(
-                          color: HexColor("#f3f3f3"),
-                          radius: 12,
-                        )
-                            : CircularProgressIndicator(
-                          color: HexColor("#f3f3f3"),
-                        ),
-                      ],
-                    );
-                  }
-                },
-              ),
-            ],
-          ),)
-        ),
+            )),
         Container(
           width: 110.w,
           height: 110.w,
@@ -301,7 +259,9 @@ class _MyRentScreenState extends State<MyRentScreen> {
     return Text(
       title,
       style: TextStyle(
-          color: HexColor("#5C7775"), fontSize: 13.5.sp, fontWeight: FontWeight.w500),
+          color: HexColor("#5C7775"),
+          fontSize: 13.5.sp,
+          fontWeight: FontWeight.w500),
     );
   }
 }
--- lib/screens/home/rent/my_rent/rent_tile.dart
+++ lib/screens/home/rent/my_rent/rent_tile.dart
@@ -1,10 +1,134 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:hexcolor/hexcolor.dart';
 import 'package:start_app/utils/common.dart';
 import '../../../../models/rent_list.dart';
 
 /// 단일 내 예약 타일 위젯
+
+class RentTile2 extends StatelessWidget {
+  RentTile2({Key? key, required this.rent, required this.onPressed})
+      : super(key: key);
+
+  Rent rent;
+  VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(children: [
+      Container(
+        width: double.infinity,
+        height: 60.h,
+        decoration: BoxDecoration(
+            color: HexColor("ffcea2"),
+            borderRadius: BorderRadius.circular(10),
+            border: Border.all(width: 2.w, color: HexColor("#ee795f"))),
+      ),
+      Row(
+        children: [
+          SizedBox(width: 16.w,),
+          Container(
+            padding: EdgeInsets.only(top: 10.h, bottom: 10.h),
+            height: 60.h,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          "이름 : ",
+                          style: TextStyle(
+                              fontSize: 15.5.sp, fontWeight: FontWeight.w500),
+                        ),
+                        Text(
+                          rent.itemCategory,
+                          style: TextStyle(
+                              fontSize: 13.5.sp, fontWeight: FontWeight.w400),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      width: 8.w,
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          "수량 : ",
+                          style: TextStyle(
+                              fontSize: 15.5.sp, fontWeight: FontWeight.w500),
+                        ),
+                        Text(
+                          rent.amount.toString(),
+                          style: TextStyle(
+                              fontSize: 13.5.sp, fontWeight: FontWeight.w400),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+                Text(
+                  "대여기간 :${Common.dateRange(rent.startTime, rent.endTime)}",
+                  style:
+                      TextStyle(fontSize: 15.5.sp, fontWeight: FontWeight.w500),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(width: 14.w,),
+          Container(
+            width: 6.w,
+            height: 36.h,
+            decoration: BoxDecoration(
+                color: HexColor("#f3f3f3"),
+                borderRadius: BorderRadius.circular(3)),
+          ),
+          SizedBox(width: 12.w,),
+          Text(
+            getKrStringFromStatus(rent.rentStatus),
+            style: TextStyle(
+                fontSize: 15.5.sp,
+                fontWeight: FontWeight.w500,
+                color: getRentColorFromStatus(rent.rentStatus)),
+          )
+        ],
+      ),
+    ]);
+  }
+
+  Color getRentColorFromStatus(String _rentStatus) {
+    if (_rentStatus == "DENY") {
+      return const Color.fromRGBO(227, 33, 38, 1);
+    } else if (_rentStatus == "WAIT") {
+      return const Color.fromRGBO(154, 166, 149, 1);
+    } else if (_rentStatus == "CONFIRM") {
+      return const Color.fromRGBO(0, 96, 10, 1);
+    } else if (_rentStatus == "RENT") {
+      return const Color.fromRGBO(10, 31, 98, 1);
+    } else {
+      return const Color.fromRGBO(70, 70, 70, 0.8);
+    }
+  }
+
+  String getKrStringFromStatus(String _rentStatus) {
+    if (_rentStatus == "DENY") {
+      return "거절";
+    } else if (_rentStatus == "WAIT") {
+      return "승인대기";
+    } else if (_rentStatus == "CONFIRM") {
+      return "승인";
+    } else if (_rentStatus == "RENT") {
+      return "대여중";
+    } else {
+      return "반납완료"; // Done
+    }
+  }
+}
+
+/// 아래 인영이꺼
 
 class RentTile extends StatelessWidget {
   RentTile({Key? key, required this.rent, required this.onPressed})
@@ -23,8 +147,6 @@ class RentTile extends StatelessWidget {
         child: Stack(alignment: Alignment.centerLeft, children: [
           SvgPicture.asset(
             "assets/rent_tile.svg",
-            width: double.infinity,
-            fit: BoxFit.fitWidth,
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -33,38 +155,60 @@ class RentTile extends StatelessWidget {
                 margin: EdgeInsets.only(left: 17.w),
                 padding: EdgeInsets.only(top: 10.h, bottom: 10.h),
                 height: 60.h,
-                child:
-                Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
                   crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    children: [
-                      Row(
-                        children: [
-                          Text("이름 : ", style: TextStyle(fontSize: 15.5.sp, fontWeight: FontWeight.w500),),
-                          Text(rent.itemCategory)
-                        ],
-                      ),
-                      SizedBox(width: 8.w,),
-                      Row(
-                        children: [
-                          Text("수량 : ", style: TextStyle(fontSize: 15.5.sp, fontWeight: FontWeight.w500),),
-                          Text(rent.amount.toString())
-                        ],
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Text("대여기간 : ", style: TextStyle(fontSize: 15.5.sp, fontWeight: FontWeight.w500),),
-                      Text(Common.dateRange(rent.startTime, rent.endTime), style: TextStyle(
-                        fontSize: 16.sp))
-                    ],
-                  ),
-                ],
-              ),),
-              Text("승인", style: TextStyle(fontSize: 15.5.sp, fontWeight: FontWeight.w500, color: checkRentStatus(rent.rentStatus)),)
+                  children: [
+                    Row(
+                      children: [
+                        Row(
+                          children: [
+                            Text(
+                              "이름 : ",
+                              style: TextStyle(
+                                  fontSize: 15.5.sp,
+                                  fontWeight: FontWeight.w500),
+                            ),
+                            Text(rent.itemCategory)
+                          ],
+                        ),
+                        SizedBox(
+                          width: 8.w,
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              "수량 : ",
+                              style: TextStyle(
+                                  fontSize: 15.5.sp,
+                                  fontWeight: FontWeight.w500),
+                            ),
+                            Text(rent.amount.toString())
+                          ],
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          "대여기간 : ",
+                          style: TextStyle(
+                              fontSize: 15.5.sp, fontWeight: FontWeight.w500),
+                        ),
+                        Text(Common.dateRange(rent.startTime, rent.endTime),
+                            style: TextStyle(fontSize: 16.sp))
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Text(
+                "승인",
+                style: TextStyle(
+                    fontSize: 15.5.sp,
+                    fontWeight: FontWeight.w500,
+                    color: getRentColorFromStatus(rent.rentStatus)),
+              )
             ],
           ),
         ]),
@@ -72,13 +216,13 @@ class RentTile extends StatelessWidget {
     );
   }
 
-  Color checkRentStatus(String _rentStatus) {
+  Color getRentColorFromStatus(String _rentStatus) {
     if (_rentStatus == "DENY") {
       return const Color.fromRGBO(227, 33, 38, 1);
     } else if (_rentStatus == "WAIT") {
       return const Color.fromRGBO(154, 166, 149, 1);
     } else if (_rentStatus == "CONFIRM") {
-      return const Color.fromRGBO(0, 96, 10, 1);
+      return HexColor("#00600f");
     } else if (_rentStatus == "RENT") {
       return const Color.fromRGBO(10, 31, 98, 1);
     } else {
