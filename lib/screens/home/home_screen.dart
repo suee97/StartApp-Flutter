05da2e91b5054c9fdc7d2c0e4363254689c488c3
suee97 <atn1su@gmail.com>
2022-08-15 18:32:59 +0900

    [FEAT] home screen api branch processing

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -3,10 +3,12 @@ import 'dart:convert';
 import 'dart:io';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:dots_indicator/dots_indicator.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hexcolor/hexcolor.dart';
+import 'package:start_app/models/status_code.dart';
 import 'package:start_app/screens/home/event/event_screen.dart';
 import 'package:start_app/screens/home/festival/festival_screen.dart';
 import 'package:start_app/screens/home/info/info_screen.dart';
@@ -18,6 +20,7 @@ import 'package:start_app/widgets/main_widget.dart';
 import '../../utils/common.dart';
 import '../../widgets/yellow_line.dart';
 import 'package:http/http.dart' as http;
+import 'dart:io' show Platform;
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -29,13 +32,10 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   double pageIndex = 0.0;
 
-  final List<String> bannerLinkList = [];
-  int linkCount = 0;
+  List<String> bannerLinkList = [];
 
-  Future<void> fetchBannerLinkList() async {
+  Future<StatusCode> fetchBannerLinkList() async {
     Map<String, dynamic> resData = {};
-    resData["status"] = 400;
-
     try {
       var resString = await http
           .get(
@@ -44,18 +44,35 @@ class _HomeScreenState extends State<HomeScreen> {
           .timeout(const Duration(seconds: 10));
       resData = jsonDecode(utf8.decode(resString.bodyBytes));
 
-      for (var e in resData["data"]) {
-        bannerLinkList.add(e["imageUrl"]);
+      if (resData["status"] == 200) {
+        List<String> tempBannerLinkList = [];
+
+        for (var e in resData["data"]) {
+          tempBannerLinkList.add(e["imageUrl"]);
+        }
+
+        bannerLinkList = tempBannerLinkList;
+        return StatusCode.SUCCESS;
       }
-      setState(() {
-        linkCount = bannerLinkList.length;
-      });
+
+      if (resData["status"] == 500) {
+        return StatusCode.SERVER_ERROR;
+      }
+
+      if (resData["status"] != 200) {
+        return StatusCode.UNCATCHED_ERROR;
+      }
+
+      return StatusCode.DEFAULT;
     } on TimeoutException catch (e) {
-      print(e);
+      print("TimeoutException : $e");
+      return StatusCode.TIMEOUT_ERROR;
     } on SocketException catch (e) {
-      print(e);
+      print("SocketException : $e");
+      return StatusCode.CONNECTION_ERROR;
     } catch (e) {
-      print(e);
+      print("UnCatchedError : $e");
+      return StatusCode.UNCATCHED_ERROR;
     }
   }
 
@@ -78,22 +95,17 @@ class _HomeScreenState extends State<HomeScreen> {
         backgroundColor: HexColor("#425C5A"),
         foregroundColor: Colors.white,
         actions: [
-          // Padding(
-          // padding: EdgeInsets.only(top: 3.0.w),
-          // child: IconButton(
           IconButton(
             onPressed: () => {
               Navigator.push(context,
                   MaterialPageRoute(builder: (context) => SettingScreen()))
             },
             icon: const Icon(Icons.settings_outlined),
-            // icon: SvgPicture.asset("assets/icon_person.svg"),
             iconSize: 24.w,
             hoverColor: Colors.transparent,
             highlightColor: Colors.transparent,
             splashColor: Colors.transparent,
           ),
-          // )
         ],
       ),
       body: Container(
@@ -113,82 +125,252 @@ class _HomeScreenState extends State<HomeScreen> {
               SizedBox(
                 height: 25.h,
               ),
-              Container(
-                  width: 280.w,
-                  height: 280.h,
-                  child: linkCount != 0
-                      ? PageView.builder(
-                          controller: PageController(
-                            initialPage: 0,
-                          ),
-                          itemCount: linkCount,
-                          itemBuilder: (BuildContext context, int index) {
-                            WidgetsBinding.instance.addPostFrameCallback((_) {
-                              setState(() {
-                                pageIndex = index.toDouble();
-                              });
-                            });
-                            return AspectRatio(
-                              aspectRatio: 1 / 1,
-                              child: CachedNetworkImage(
-                                imageUrl: bannerLinkList[index],
-                                placeholder: (context, url) => Center(
-                                    child: CircularProgressIndicator(
-                                  color: HexColor("#425c5a"),
-                                )),
-                                errorWidget: (context, url, error) => Center(
-                                  child: AspectRatio(
-                                    aspectRatio: 1 / 1,
-                                    child: Container(
-                                      width: double.infinity,
-                                      decoration: BoxDecoration(
-                                          color: HexColor("#c4c4c4"),
-                                          borderRadius:
-                                              BorderRadius.circular(8)),
-                                      child: Column(
-                                        mainAxisSize: MainAxisSize.min,
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                        children: [
-                                          const Image(
-                                              image: AssetImage(
-                                                  "images/logo_crying_ready.png")),
-                                          Text(
-                                            "이미지를 불러오지 못했습니다.",
-                                            style: TextStyle(
-                                                fontSize: 16.sp,
-                                                fontWeight: FontWeight.w600,
-                                                color: HexColor("#6c6c6c")),
-                                          )
-                                        ],
+              FutureBuilder(
+                future: fetchBannerLinkList(),
+                builder: (BuildContext context, AsyncSnapshot snapshot) {
+                  if (snapshot.data == StatusCode.SUCCESS) {
+                    return Column(
+                      children: [
+                        Container(
+                            width: 280.w,
+                            height: 280.h,
+                            child: PageView.builder(
+                              controller: PageController(
+                                initialPage: 0,
+                              ),
+                              itemCount: bannerLinkList.length,
+                              itemBuilder: (BuildContext context, int index) {
+                                WidgetsBinding.instance
+                                    .addPostFrameCallback((_) {
+                                  setState(() {
+                                    pageIndex = index.toDouble();
+                                  });
+                                });
+                                return AspectRatio(
+                                  aspectRatio: 1 / 1,
+                                  child: CachedNetworkImage(
+                                    imageUrl: bannerLinkList[index],
+                                    placeholder: (context, url) => Center(
+                                        child: CircularProgressIndicator(
+                                      color: HexColor("#425c5a"),
+                                    )),
+                                    errorWidget: (context, url, error) =>
+                                        Center(
+                                      child: AspectRatio(
+                                        aspectRatio: 1 / 1,
+                                        child: Container(
+                                          width: double.infinity,
+                                          decoration: BoxDecoration(
+                                              color: HexColor("#c4c4c4"),
+                                              borderRadius:
+                                                  BorderRadius.circular(8)),
+                                          child: Column(
+                                            mainAxisSize: MainAxisSize.min,
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.center,
+                                            children: [
+                                              const Image(
+                                                  image: AssetImage(
+                                                      "images/logo_crying_ready.png")),
+                                              Text(
+                                                "이미지를 불러오지 못했습니다.",
+                                                style: TextStyle(
+                                                    fontSize: 16.sp,
+                                                    fontWeight: FontWeight.w600,
+                                                    color: HexColor("#6c6c6c")),
+                                              )
+                                            ],
+                                          ),
+                                        ),
                                       ),
                                     ),
                                   ),
-                                ),
+                                );
+                              },
+                            )),
+                        SizedBox(
+                          height: 4.h,
+                        ),
+                        Container(
+                            height: 20.h,
+                            child: DotsIndicator(
+                              dotsCount: bannerLinkList.length,
+                              position: pageIndex.toDouble(),
+                              decorator: DotsDecorator(
+                                activeColor: HexColor("#EE795F"),
                               ),
-                            );
-                          },
-                        )
-                      : Container(
-                          child: Center(
-                            child: CircularProgressIndicator(
-                              color: HexColor("#425c5a"),
+                            )),
+                      ],
+                    );
+                  } else if (snapshot.data == StatusCode.SERVER_ERROR) {
+                    return Column(
+                      children: [
+                        Container(
+                          width: 280.w,
+                          height: 280.h,
+                          child: AspectRatio(
+                            aspectRatio: 1 / 1,
+                            child: Container(
+                              width: double.infinity,
+                              decoration: BoxDecoration(
+                                  color: HexColor("#c4c4c4"),
+                                  borderRadius: BorderRadius.circular(8)),
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  const Image(
+                                      image: AssetImage(
+                                          "images/logo_crying_ready.png")),
+                                  Text(
+                                    "이미지를 가져오지 못했습니다.",
+                                    style: TextStyle(
+                                        fontSize: 16.sp,
+                                        fontWeight: FontWeight.w600,
+                                        color: HexColor("#6c6c6c")),
+                                  )
+                                ],
+                              ),
                             ),
                           ),
-                        )),
-              SizedBox(
-                height: 4.h,
-              ),
-              Container(
-                  child: linkCount != 0
-                      ? DotsIndicator(
-                          dotsCount: linkCount,
-                          position: pageIndex.toDouble(),
-                          decorator: DotsDecorator(
-                            activeColor: HexColor("#EE795F"),
-                          ),
+                        ),
+                        SizedBox(
+                          height: 24.h,
                         )
-                      : Container(height: 20.h)),
+                      ],
+                    );
+                  } else if (snapshot.data == StatusCode.UNCATCHED_ERROR) {
+                    return Column(
+                      children: [
+                        Container(
+                          width: 280.w,
+                          height: 280.h,
+                          child: AspectRatio(
+                            aspectRatio: 1 / 1,
+                            child: Container(
+                              width: double.infinity,
+                              decoration: BoxDecoration(
+                                  color: HexColor("#c4c4c4"),
+                                  borderRadius: BorderRadius.circular(8)),
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  const Image(
+                                      image: AssetImage(
+                                          "images/logo_crying_ready.png")),
+                                  Text(
+                                    "이미지를 가져오지 못했습니다.",
+                                    style: TextStyle(
+                                        fontSize: 16.sp,
+                                        fontWeight: FontWeight.w600,
+                                        color: HexColor("#6c6c6c")),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 24.h,
+                        )
+                      ],
+                    );
+                  } else if (snapshot.data == StatusCode.TIMEOUT_ERROR) {
+                    return Column(
+                      children: [
+                        Container(
+                          width: 280.w,
+                          height: 280.h,
+                          child: AspectRatio(
+                            aspectRatio: 1 / 1,
+                            child: Container(
+                              width: double.infinity,
+                              decoration: BoxDecoration(
+                                  color: HexColor("#c4c4c4"),
+                                  borderRadius: BorderRadius.circular(8)),
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  const Image(
+                                      image: AssetImage(
+                                          "images/logo_crying_ready.png")),
+                                  Text(
+                                    "이미지를 가져오지 못했습니다.",
+                                    style: TextStyle(
+                                        fontSize: 16.sp,
+                                        fontWeight: FontWeight.w600,
+                                        color: HexColor("#6c6c6c")),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 24.h,
+                        )
+                      ],
+                    );
+                  } else if (snapshot.data == StatusCode.CONNECTION_ERROR) {
+                    return Column(
+                      children: [
+                        Container(
+                          width: 280.w,
+                          height: 280.h,
+                          child: AspectRatio(
+                            aspectRatio: 1 / 1,
+                            child: Container(
+                              width: double.infinity,
+                              decoration: BoxDecoration(
+                                  color: HexColor("#c4c4c4"),
+                                  borderRadius: BorderRadius.circular(8)),
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  const Image(
+                                      image: AssetImage(
+                                          "images/logo_crying_ready.png")),
+                                  Text(
+                                    "네트워크 연결상태를 확인해주세요.",
+                                    style: TextStyle(
+                                        fontSize: 16.sp,
+                                        fontWeight: FontWeight.w600,
+                                        color: HexColor("#6c6c6c")),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 24.h,
+                        )
+                      ],
+                    );
+                  } else {
+                    return Column(
+                      children: [
+                        Container(
+                            width: 280.w,
+                            height: 280.h,
+                            child: Platform.isIOS
+                                ? CupertinoActivityIndicator()
+                                : Center(
+                                    child: CircularProgressIndicator(
+                                      color: HexColor("#425c5a"),
+                                    ),
+                                  )),
+                        SizedBox(
+                          height: 24.h,
+                        )
+                      ],
+                    );
+                  }
+                },
+              ),
               Column(children: [
                 const YellowLine(),
                 SizedBox(
