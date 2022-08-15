3159ee08d99fb0497efc1ad10b3523a32e105581
suee97 <atn1su@gmail.com>
2022-08-16 03:05:31 +0900

    [FEAT] alert dialog

--- lib/screens/login/login_option_screen.dart
+++ lib/screens/login/login_option_screen.dart
@@ -3,8 +3,8 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hexcolor/hexcolor.dart';
 import '../../utils/common.dart';
 import '../home/home_screen.dart';
-import 'login_nav_button.dart';
-import 'sign_in/login_screen.dart';
+import 'login_widgets.dart';
+import 'login_screen.dart';
 
 class LoginOptionScreen extends StatelessWidget {
   const LoginOptionScreen({Key? key}) : super(key: key);
@@ -21,7 +21,7 @@ class LoginOptionScreen extends StatelessWidget {
             SizedBox(
               height: 220.h,
             ),
-            Container(
+            SizedBox(
               width: 200.w,
               height: 200.h,
               child: const Image(
@@ -32,26 +32,131 @@ class LoginOptionScreen extends StatelessWidget {
               height: 70.h,
             ),
             LoginNavButton(
-                colorHex: "#425c5a",
-                title: '로그인',
-                onPressed: () => {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => LoginScreen()))
-                    }),
+              colorHex: "#425c5a",
+              title: '로그인',
+              onPressed: () => {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => LoginScreen()))
+              },
+              width: 330.w,
+            ),
             SizedBox(
               height: 12.h,
             ),
             LoginNavButton(
-                colorHex: "#929d9c",
-                title: '로그인 없이 이용하기',
-                onPressed: () {
-                  Common.setNonLogin(true);
-                  Navigator.of(context).pushAndRemoveUntil(
-                      MaterialPageRoute(builder: (context) => HomeScreen()),
-                      (route) => false);
-                }),
+              colorHex: "#929d9c",
+              title: '로그인 없이 이용하기',
+              onPressed: () {
+                showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        backgroundColor: HexColor("#F8EAE1"),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30)),
+                        title: Text(
+                          "로그인 없이 이용하기",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontSize: 24.sp, fontWeight: FontWeight.w500),
+                        ),
+                        content: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "로그인을 하지 않을 시 다음 사항에 제약이 있을 수 있습니다.",
+                              style: TextStyle(
+                                  fontSize: 15.5.sp,
+                                  fontWeight: FontWeight.w300),
+                            ),
+                            SizedBox(
+                              height: 26.h,
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(left: 16.w),
+                              child: Text(
+                                "∙ 주 사업 이벤트",
+                                style: TextStyle(
+                                    fontSize: 17.sp,
+                                    fontWeight: FontWeight.w300),
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(left: 16.w),
+                              child: Text(
+                                "∙ 상시사업 예약 및 예약확인",
+                                style: TextStyle(
+                                    fontSize: 17.sp,
+                                    fontWeight: FontWeight.w300),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 28.h,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                GestureDetector(
+                                  onTap: () {
+                                    Common.setNonLogin(true);
+                                    Navigator.of(context).pushAndRemoveUntil(
+                                        MaterialPageRoute(builder: (context) => HomeScreen()),
+                                            (route) => false);
+                                  },
+                                  child: Container(
+                                    width: 114.w,
+                                    height: 40.h,
+                                    alignment: Alignment.center,
+                                    decoration: BoxDecoration(
+                                        color: HexColor("#425c5a"),
+                                        borderRadius: const BorderRadius.only(
+                                            topLeft: Radius.circular(10),
+                                            bottomLeft: Radius.circular(10))),
+                                    child: Text(
+                                      "확인",
+                                      style: TextStyle(
+                                          fontSize: 19.5.sp,
+                                          fontWeight: FontWeight.w300,
+                                          color: Colors.white),
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: 6.w,
+                                ),
+                                GestureDetector(
+                                  onTap: () {
+                                    Navigator.pop(context);
+                                  },
+                                  child: Container(
+                                    width: 114.w,
+                                    height: 40.h,
+                                    alignment: Alignment.center,
+                                    decoration: BoxDecoration(
+                                        color: HexColor("#425c5a"),
+                                        borderRadius: const BorderRadius.only(
+                                            topRight: Radius.circular(10),
+                                            bottomRight: Radius.circular(10))),
+                                    child: Text(
+                                      "취소",
+                                      style: TextStyle(
+                                          fontSize: 19.5.sp,
+                                          fontWeight: FontWeight.w300,
+                                          color: Colors.white),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      );
+                    });
+              },
+              width: 330.w,
+            ),
           ],
         ),
       ),
