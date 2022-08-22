76bb2b68fae077c81161a481467b5947a310388b
suee97 <atn1su@gmail.com>
2022-08-23 02:49:16 +0900

    [REFACTOR] rent list model 수정

--- lib/models/rent_list.dart
+++ lib/models/rent_list.dart
@@ -7,23 +7,37 @@ class RentList {
 }
 
 class Rent {
-  Rent(this.rentId, this.itemCategory,
-      this.startTime, this.endTime, this.amount, this.rentStatus);
+  Rent(
+      this.rentId,
+      this.account,
+      this.purpose,
+      this.rentStatus,
+      this.itemCategory,
+      this.startTime,
+      this.endTime,
+      this.createdAt,
+      this.updatedAt);
 
   int rentId;
+  int account;
+  String purpose;
+  String rentStatus;
   String itemCategory;
   String startTime;
   String endTime;
-  int amount;
-  String rentStatus;
+  String createdAt;
+  String updatedAt;
 
   factory Rent.fromJson(Map<String, dynamic> jsonData) {
     return Rent(
         jsonData["rentId"],
+        jsonData["account"],
+        jsonData["purpose"],
+        jsonData["rentStatus"],
         jsonData["itemCategory"],
         jsonData["startTime"],
         jsonData["endTime"],
-        jsonData["amount"],
-        jsonData["rentStatus"]);
+        jsonData["createdAt"],
+        jsonData["updatedAt"]);
   }
 }
