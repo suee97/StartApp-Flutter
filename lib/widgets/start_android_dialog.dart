c40ba8fbb15834a292fab51c15f61ac80bac9c0b
suee97 <atn1su@gmail.com>
2022-08-23 16:33:34 +0900

    [DESIGN] 다이얼로그

--- lib/widgets/start_android_dialog.dart
+++ lib/widgets/start_android_dialog.dart
@@ -20,55 +20,58 @@ class StartAndroidDialog extends StatelessWidget {
       title: Text(
         title,
         style: TextStyle(
-            fontSize: 20.sp,
+            fontSize: 18.sp,
             fontWeight: FontWeight.w500,
             color: HexColor("#425C5A")),
       ),
-      content: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          GestureDetector(
-            onTap: onOkPressed,
-            child: Container(
-              width: 114.w,
-              height: 40.h,
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                  color: HexColor("#425c5a"),
-                  borderRadius: const BorderRadius.only(
-                      topLeft: Radius.circular(10),
-                      bottomLeft: Radius.circular(10))),
-              child: Text(
-                "확인",
-                style: TextStyle(
-                    fontSize: 19.5.sp,
-                    fontWeight: FontWeight.w400,
-                    color: Colors.white),
+      content: Padding(
+        padding: EdgeInsets.only(top: 24.h),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            GestureDetector(
+              onTap: onOkPressed,
+              child: Container(
+                width: 114.w,
+                height: 40.h,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                    color: HexColor("#425c5a"),
+                    borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(10),
+                        bottomLeft: Radius.circular(10))),
+                child: Text(
+                  "확인",
+                  style: TextStyle(
+                      fontSize: 18.sp,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white),
+                ),
               ),
             ),
-          ),
-          SizedBox(width: 6.w,),
-          GestureDetector(
-            onTap: onCancelPressed,
-            child: Container(
-              width: 114.w,
-              height: 40.h,
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                  color: HexColor("#425c5a"),
-                  borderRadius: const BorderRadius.only(
-                      topRight: Radius.circular(10),
-                      bottomRight: Radius.circular(10))),
-              child: Text(
-                "취소",
-                style: TextStyle(
-                    fontSize: 19.5.sp,
-                    fontWeight: FontWeight.w400,
-                    color: Colors.white),
+            SizedBox(width: 6.w,),
+            GestureDetector(
+              onTap: onCancelPressed,
+              child: Container(
+                width: 114.w,
+                height: 40.h,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                    color: HexColor("#425c5a"),
+                    borderRadius: const BorderRadius.only(
+                        topRight: Radius.circular(10),
+                        bottomRight: Radius.circular(10))),
+                child: Text(
+                  "취소",
+                  style: TextStyle(
+                      fontSize: 18.sp,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white),
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
       shape: const RoundedRectangleBorder(
           borderRadius: BorderRadius.all(Radius.circular(20))),
