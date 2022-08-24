06c85f37c6f405e7d42be6c56eab3d5f6bc4bc75
KangInyeong <kiyoog02@gmail.com>
2022-08-24 10:51:26 +0900

    [FIX] festival lineup logic & festival stamp button loading

--- lib/screens/home/festival/stamp_button.dart
+++ lib/screens/home/festival/stamp_button.dart
@@ -1,12 +1,15 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hexcolor/hexcolor.dart';
+import 'dart:io' show Platform;
 
 class StampButton extends StatelessWidget {
-  StampButton({Key? key, required this.title})
+  StampButton({Key? key, required this.title, required this.loading})
       : super(key: key);
 
   String title;
+  bool loading;
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +17,15 @@ class StampButton extends StatelessWidget {
       width: 250.w,
       height: 40.h,
       alignment: Alignment.center,
-      child: Text(
+      child: loading ? Center(
+        child: Platform.isIOS
+            ? const CupertinoActivityIndicator(
+          color: Colors.white,
+        )
+            : CircularProgressIndicator(
+          color: HexColor("#f3f3f3"),
+        ),
+      ) : Text(
         title,
         style: TextStyle(
             fontSize: 21.5.sp,
