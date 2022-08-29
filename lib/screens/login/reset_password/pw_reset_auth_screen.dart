921e0bd404affcaad1c0ffe0b94643c966d91698
suee97 <atn1su@gmail.com>
2022-08-30 00:35:29 +0900

    [DESIGN] 멘트수정

--- lib/screens/login/reset_password/pw_reset_auth_screen.dart
+++ lib/screens/login/reset_password/pw_reset_auth_screen.dart
@@ -150,7 +150,7 @@ class _PwResetAuthScreenState extends State<PwResetAuthScreen> {
                                           PostSmsByIdCode.ST058) {
                                         if (!mounted) return;
                                         Common.showSnackBar(
-                                            context, "탈퇴한 학생입니다.");
+                                            context, "탈퇴한 계정입니다. 재가입시 문의해주세요. (02");
                                         return;
                                       }
                                       if (!mounted) return;
