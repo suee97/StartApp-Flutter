b57927fb03a3067c97732d7fe79bfb683c1ce594
suee97 <atn1su@gmail.com>
2022-09-06 00:47:13 +0900

    [FEAT] common loading widget

--- lib/screens/login/login_widgets.dart
+++ lib/screens/login/login_widgets.dart
@@ -1,26 +1,30 @@
+import 'dart:io' show Platform;
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hexcolor/hexcolor.dart';
 
 class LoginNavButton extends StatelessWidget {
-  LoginNavButton(
-      {Key? key,
-        required this.onPressed,
-        required this.title,
-        required this.colorHex,
-        required this.width,
-        this.margin
-      })
-      : super(key: key);
+  LoginNavButton({
+    Key? key,
+    required this.onPressed,
+    required this.title,
+    required this.colorHex,
+    required this.width,
+    this.margin = EdgeInsets.zero,
+    this.isLoading = false,
+  }) : super(key: key);
 
   VoidCallback onPressed;
   String title;
   String colorHex;
   double width;
-  EdgeInsets? margin = EdgeInsets.zero;
+  EdgeInsets margin;
+  bool isLoading;
 
   @override
   Widget build(BuildContext context) {
+    print(isLoading.toString());
     return GestureDetector(
       onTap: onPressed,
       child: Container(
@@ -28,17 +32,25 @@ class LoginNavButton extends StatelessWidget {
         margin: margin,
         height: 54.h,
         decoration: BoxDecoration(
-            color: HexColor(colorHex),
-            borderRadius: BorderRadius.circular(10)
-        ),
+            color: HexColor(colorHex), borderRadius: BorderRadius.circular(10)),
         alignment: Alignment.center,
-        child: Text(
-          title,
-          style: TextStyle(
-              fontSize: 19.5.sp,
-              fontWeight: FontWeight.w500,
-              color: Colors.white),
-        ),
+        child: isLoading == false
+            ? Text(
+                title,
+                style: TextStyle(
+                    fontSize: 19.5.sp,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white),
+              )
+            : Center(
+                child: Platform.isIOS == true
+                    ? CupertinoActivityIndicator(
+                        color: HexColor("#f3f3f3"),
+                      )
+                    : CircularProgressIndicator(
+                        color: HexColor("#f3f3f3"),
+                      ),
+              ),
       ),
     );
   }
@@ -55,10 +67,7 @@ class LoginInputField extends StatelessWidget {
       height: 24.h,
       child: TextField(
         controller: controller,
-        style: TextStyle(
-            fontSize: 17.5.sp,
-            fontWeight: FontWeight.w300
-        ),
+        style: TextStyle(fontSize: 17.5.sp, fontWeight: FontWeight.w300),
         decoration: InputDecoration(
           enabledBorder: UnderlineInputBorder(
             borderSide: BorderSide(color: HexColor("#425c5a")),
